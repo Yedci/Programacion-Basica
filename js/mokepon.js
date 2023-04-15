42a049ee0035fddae0f37874b2add22480aa69f6
Yesica Galindez <yessy3497@gmail.com}>
2023-04-15 17:19:10 +0200

    ocultando elementos HTML con JS para mejorar la UX del juego

--- js/mokepon.js
+++ js/mokepon.js
@@ -7,6 +7,14 @@ let vidasEnemigo=3
 
 
 function iniciarJuego(){
+    // ocultando elementos HTMl con js  funcion style.display
+    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+    sectionSeleccionarAtaque.style.display='none'
+
+    let sectionReiniciar = document.getElementById("reiniciar")
+    sectionReiniciar.style.display='none'
+
+    // leyendo eventos al momento de realizar in click en los botones
     let botonMascotaJugador= document.getElementById('boton-mascota')
     botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador) 
 
@@ -22,6 +30,16 @@ function iniciarJuego(){
 }
 
 function seleccionarMascotaJugador(){
+
+    let sectionSeleccionarMascota=document.getElementById("seleccionar-mascota")
+    sectionSeleccionarMascota.style.display="none"
+
+
+    let sectionSeleccionarAtaque = document.getElementById("seleccionar-ataque")
+    sectionSeleccionarAtaque.style.display='block'
+
+    
+
     let inputHipodoge=document.getElementById('hipodoge')
     let inputCapipepo=document.getElementById('capipepo')
     let inputRatigueya=document.getElementById('ratigueya')
@@ -134,6 +152,9 @@ function crearMensajeFinal(resultadoFinal){
     let botonTierra=document.getElementById('boton-tierra')
     botonTierra.disabled=true
 
+    let sectionReiniciar = document.getElementById("reiniciar")
+    sectionReiniciar.style.display='block'
+
 }
 function reiniciarJuego(){
     location.reload()
@@ -142,4 +163,4 @@ function reiniciarJuego(){
 function aleatorio(min, max){
     return Math.floor(Math.random() * (max-min +1)+ min)
 }
-window.addEventListener('load',iniciarJuego)                                                                
+window.addEventListener('load',iniciarJuego)                                                            
